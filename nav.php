cf67803bf8eddfd78bdec4a1246cd293eb8bfe58
Julia <julia.yan@hotmail.com>
2018-07-03 23:34:27 -0400

    fixed navigation

--- nav.php
+++ nav.php
@@ -1,14 +1,25 @@
 <ul class="nav">
   <li class="nav-item">
-    <a class="nav-link active" href="#">Active</a>
+    <a class="nav-link active" href="#">Home</a>
+  </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">About Us</a>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="./locations.php">Location and Hours</a>
+      <a class="dropdown-item" href="./holiday_hours.php">Holiday Hours</a>
+      <a class="dropdown-item" href="./membership.php">Membership</a>
+      <a class="dropdown-item" href="./fines.php">Fines</a>
+      <a class="dropdown-item" href="./supporting.php">Support</a>
+      <a class="dropdown-item" href="./contact_us.php">Contact Us</a>
+    </div>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
+    <a class="nav-link" href="./catalogue.php">Catalogue</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
+    <a class="nav-link" href="#">Programs</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link disabled" href="#">Disabled</a>
+    <a class="nav-link" href="#">Services</a>
   </li>
 </ul>
